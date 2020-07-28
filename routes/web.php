b847a566f43b7779150843dde9d384e013333e44
AkinoMush1 <396870249@qq.com>
2020-07-28 22:53:16 +0800

    用户界面 订单列表页

--- routes/web.php
+++ routes/web.php
@@ -11,26 +11,32 @@
 |
 */
 
-//Route::get('/', 'PagesController@root')->name('root');
 Route::redirect('/', '/products')->name('root');
 Auth::routes(['verify' => true]);
 
+// 商品列表
 Route::get('products', 'ProductsController@index')->name('products.index');
+
+// 商品收藏
 Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
 Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
 
 Route::group(['middleware' => ['auth', 'verified']], function () {
-//    Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
-//    Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
-//    Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');
-//    Route::get('user_addresses/{user_address}', 'UserAddressesController@edit')->name('user_addresses.edit');
-//    Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');
-//    Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
+    // 收货地址
     Route::resource('user_addresses', 'UserAddressesController');
+
+    // 商品收藏
     Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
+
+    // 购物车
     Route::post('cart', 'CartController@add')->name('cart.add');
     Route::get('cart', 'CartController@index')->name('cart.index');
     Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
+
+    // 订单
     Route::post('orders', 'OrdersController@store')->name('orders.store');
+    Route::get('orders', 'OrdersController@index')->name('orders.index');
 });
+
+// 商品详细
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
