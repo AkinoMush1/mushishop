8d87ad4051de5ab7c8c188e488a980cccc6bd4ac
AkinoMush1 <396870249@qq.com>
2020-07-30 11:09:13 +0800

    管理后台 订单详细

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -14,6 +14,7 @@ Route::group([
     $router->get('users', 'UsersController@index');
     $router->resource('products', 'ProductsController');
     $router->get('orders', 'OrdersController@index')->name('admin.orders.index');
+    $router->get('orders/{order}', 'OrdersController@show')->name('admin.orders.show');
 //    $router->get('products', 'ProductsController@index');
 //    $router->get('products/create', 'ProductsController@create');
 //    $router->post('products', 'ProductsController@store');
