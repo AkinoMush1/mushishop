ac81d3d1671d38fdeaa0d6c33059375d2bdf092e
AkinoMush1 <396870249@qq.com>
2020-08-09 14:30:41 +0800

    Elasticsearch 索引迁移命令

--- app/Console/Commands/Elasticsearch/SyncProducts.php
+++ app/Console/Commands/Elasticsearch/SyncProducts.php
@@ -12,7 +12,7 @@ class SyncProducts extends Command
      *
      * @var string
      */
-    protected $signature = 'es:sync-products';
+    protected $signature = 'es:sync-products {--index=products}';
 
     /**
      * The console command description.
@@ -50,7 +50,8 @@ class SyncProducts extends Command
                     $data = $product->toESArray();
                     $req['body'][] = [
                         'index' => [
-                            '_index' => 'products',
+                            // 从参数中读取索引名称
+                            '_index' => $this->option('index'),
                             '_id'    => $data['id'],
                         ],
                     ];
